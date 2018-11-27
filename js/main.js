94312fe65779925d91b22830e54f6fb378496e2e
stp8954 <sanket.t.patel@outlook.com>
2018-11-26 16:14:25 -0800

    prototype fiest few charts

--- js/main.js
+++ js/main.js
@@ -1,65 +1,127 @@
-  var multi_chart_wage =
-  {
-    "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
-    "description": "Google's stock price over time.",
-    "width": 500,
-    "height": 100,
-    "data": {
-        "name":"myData"
-     },
-    "mark": "line",
-    "encoding": {
-      "x": {"field": "year", "type": "temporal"},
-      "y": {"field": "A_MEDIAN", "type": "quantitative"}
-    }
-  };
-
-  var multi_chart_rpp =
-  {
-    "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
-    "description": "Google's stock price over time.",
-    "width": 500,
-    "height": 100,
-    "data": {
-        "name":"myData"
-     },
-    "mark": "line",
-    "encoding": {
-      "x": {"field": "year", "type": "temporal"},
-      "y": {"field": "CPI", "type": "quantitative"}
-    }
-  };
   
- var stacked_charts = {
+var stacked_charts = {
     "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
-    "title": "Seattle Weather, 2012-2015",
+    "title": "State Wage and RPP data",
     "data": {
-      "name":"myData"
+        "name":"wageData"
     },
+    "transform": [
+        {
+            "filter": {
+                "and": [
+                    "datum.year == 2016"
+                ]
+            }
+        }
+    ],
+    "vconcat": [
+        {
+            "encoding": {
+                "x": {
+                    "axis": { "title": "Date"},
+                    "field": "year",
+                    "type": "temporal"
+                },
+                "y": {
+                    "axis": { "title": "Median Salary" },
+                    "field": "A_MEDIAN",
+                    "scale": { "domain": [0, 200000] },
+                    "type": "quantitative"
+                }
+            },
+            "width": 600,
+            "mark": { "type": "line", "color": "green", "point": { "color": "red" } },
+            "selection": { "brush": { "encodings": ["x"], "type": "interval" } },
+        }, {
+            "encoding": {
+                "x": {
+                    "axis": { "title": "Date" },
+                    "field": "year",
+                    "type": "temporal"
+                },
+                "y": {
+                    "axis": { "title": "Median Salary" },
+                    "field": "CPI",
+                    "scale": { "domain": [50, 150] },
+                    "type": "quantitative"
+                }
+            },
+            "width": 600,
+            "mark": { "type": "line", "color": "green", "point": { "color": "red" } },
+            "transform": [{ "filter": { "selection": "brush" } }]
+        }, {
+            "encoding": {
+                "x": {
+                    "axis": { "title": "Date" },
+                    "field": "year",
+                    "type": "temporal"
+                },
+                "y": {
+                    "axis": { "title": "Adjusted Median Salary" },
+                    "field": "A_MEDIAN_ADJ",
+                    "scale": { "domain": [0, 200000] },
+                    "type": "quantitative"
+                }
+            },
+            "width": 600,
+            "mark": { "type": "line", "color": "green", "point": { "color": "red" } },
+            "transform": [{ "filter": { "selection": "brush" } }]
+        }
+    ]
+}
 
- }
+var occ_rank_chart = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
+    "description": "A bar graph showing the scores of the top 5 students. This shows an example of the window transform, for how the top K (5) can be filtered, and also how a rank can be computed for each student.",
+    "data": {
+        "name":"wageData"
+    },
+    "width": 600,
+    "transform": [
+        {
+            "window": [{
+                "op": "rank",
+                "as": "rank"
+            }],
+            "sort": [{ "field": "A_MEDIAN_ADJ", "order": "descending" }]
+        }, {
+            "filter": {
+                "and": [
+                    "datum.year == '2016'",
+                    "datum.rank <= 100"
+                ]
+            } 
+        }
+    ],
+    "mark": "bar",
+    "encoding": {
+        "x": {
+            "field": "A_MEDIAN_ADJ",
+            "type": "quantitative"
+        },
+        "y": {
+            "field": "OCC_TITLE",
+            "type": "nominal",
+            "sort": { "field": "A_MEDIAN_ADJ", "order": "descending" }
+        }
+    }
+}
 
-function join(lookupTable, mainTable, lookupKey, mainKey, select) {
-    var l = lookupTable.length,
-        m = mainTable.length,
-        lookupIndex = [],
-        output = [];
-    for (var i = 0; i < l; i++) { // loop through l items
-        var row = lookupTable[i];
-        lookupIndex[row[lookupKey]] = row; // create an index for lookup table
-    }
-    for (var j = 0; j < m; j++) { // loop through m items
-        var y = mainTable[j];
-        var x = lookupIndex[y[mainKey]]; // get corresponding row from lookupTable
-        output.push(select(y, x)); // select only the columns you need
-    }
-    return output;
-};
 
 function filterOcc(data, state, occupation){
 
     return data.filter(function(d){
         if (d.ST == state & d.OCC_TITLE == occupation ){
+            return true;
+        }
+        return false;
+    });
+}
+
+function filterOcc(data, state) {
+
+    return data.filter(function (d) {
+        if (d.ST == state ) {
             return true;
         }
         return false;
@@ -83,30 +145,30 @@ var st = "WA"
 var state = 'Washington'
 
 function updateCharts(){
-    var state_wage_data = filterOcc(wage_data,st,"Software Developers Applications");
-    var state_rpp_data = filterRpp(rpp_data,state);
+    //var state_wage_data = filterOcc(wage_data,st,"Software Developers Applications");
+    var state_wage_data = filterOcc(wage_data, st);
+    //var state_rpp_data = filterRpp(rpp_data,state);
 
-    var result = join(state_rpp_data, state_wage_data, "State","STATE", function(wage, rpp){
+    // vegaEmbed("#salary", multi_chart_wage)
+    //     .then((res) => res.view
+    //         .insert("wageData", state_wage_data)
+    //     .run()
+    // );
 
-        return {
-            "ST":wage.ST,
-            "State":rpp.State,
-            "A_MEDIAN": wage.A_MEDIAN,
-            "CPI": rpp.CPI
-        }
-    });
-    
-    vegaEmbed("#salary", multi_chart_wage)
+    // vegaEmbed("#cpi", multi_chart_rpp)
+    //     .then((res) => res.view
+    //         .insert("myData", state_wage_data)
+    //     .run()
+    // );
+
+    vegaEmbed("#salary", occ_rank_chart)
         .then((res) => res.view
-        .insert("myData", state_wage_data)
-        .run()
-    );
-
-    vegaEmbed("#cpi", multi_chart_rpp)
-        .then((res) => res.view
-        .insert("myData", state_rpp_data)
-        .run()
-    );
+            .insert("wageData", state_wage_data)
+            .addEventListener('click', function (event, item) {
+                console.log(item)
+            })
+            .run()
+        );
 }
 
 
@@ -118,18 +180,16 @@ function updateCharts(){
 
 
   d3.queue()
-  .defer(d3.csv, "/data/rpp_2007_2016.csv")
+  //.defer(d3.csv, "/data/rpp_2007_2016.csv")
   .defer(d3.csv, "/data/wages_2007_2016_2.csv")
   .await(analyze);
 
 
-function analyze(error, rpp, wages){
+function analyze(error, wages){
   
     if(error){console.log(error);}
 
     wage_data = wages;
-    rpp_data = rpp;
-
     updateCharts();
 };
 
